40449d463fc4057f3dd5fd8984c9187212f52b9e
BryanG <davidbryanrs@gmail.com>
2024-07-02 15:20:01 -0500

    feat(app): implement header component to use globally in every component

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import 'primeicons/primeicons.css'
 
 import router from './router'
 
+import HeaderComponent from '@/components/Header.component.vue'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
+
 createApp(App)
+    .component("page-header", HeaderComponent)
     .use(router)
     .mount('#app')
