43f215663307f1d7d65019aaa6d9f600e71e2bc4
BryanG <davidbryanrs@gmail.com>
2024-07-02 22:04:13 -0500

    feat(page): change style of the page and add more info

--- src/pages/Home.page.vue
+++ src/pages/Home.page.vue
@@ -1,13 +1,22 @@
 <script setup>
-
 </script>
 
 <template>
   <div></div>
-  <page-header/>
-  <main class = "flex mt-16 text-2xl flex-col items-center">
-    <h1>Blogify page</h1>
-    <p>WIP</p>
+  <main class = "flex mt-24 w-page-limit min-w-72 flex-col gap-10">
+    <div>
+      <h1 class = "text-4xl">Home</h1>
+      <hr class = "my-5 border-gray-500">
+      <p>¡Welcome to this page!</p>
+    </div>
+    <div class = "text-1xl flex gap-2.5 items-center">
+      <i class = "pi pi-spin pi-spinner"/>
+      <p>Page in progress...</p>
+
+    </div>
+
+
+
   </main>
 
 </template>
