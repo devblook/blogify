aa225012684e36f5d5a1db709e78976bb572e1bf
BryanG <davidbryanrs@gmail.com>
2024-07-02 22:05:24 -0500

    feat(option-button): create component

---
+++ src/components/OptionButton.component.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class = "bg-gray-200 hover:bg-gray-300 active:bg-gray-400 cursor-pointer rounded-lg py-1.5 px-2.5">
+    <slot/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
