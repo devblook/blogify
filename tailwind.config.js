52d01cf0fd3570fdd08d80b63e7c16051ca08ded
BryanG <davidbryanrs@gmail.com>
2024-07-02 19:37:35 -0500

    chore(styles): add styles classes to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        'header': '25em'
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif']
+      },
+      fontSize: {
+        'header':'1.6em'
+      }
+    },
   },
   plugins: [],
 }
