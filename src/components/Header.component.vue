2d37980e8e2bc95d7a79b13226bddeebfa45564e
BryanG <davidbryanrs@gmail.com>
2024-07-02 19:36:12 -0500

    feat(header): change color and add theme mode and language button

--- src/components/Header.component.vue
+++ src/components/Header.component.vue
@@ -3,8 +3,14 @@
 </script>
 
 <template>
-  <div class = "bg-blue-400 flex p-3 justify-center">
-    <h1>Header</h1>
+  <div class = "font-inter text-header mt-10 bg-red-400 w-header py-3.5 px-3 rounded-xl flex items-center justify-between">
+    <h1>BlogIn</h1>
+    <p>Home</p>
+    <div class = "flex gap-2 items-center">
+      <i class = "text-xl pi pi-sun"/>
+      <h1>es</h1>
+
+    </div>
   </div>
 </template>
 
