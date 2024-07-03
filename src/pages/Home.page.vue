eac3bad1365276f4e40ded4b0729232ba9b94a47
BryanG <davidbryanrs@gmail.com>
2024-07-02 19:36:27 -0500

    refactor(page): change page name

--- src/pages/Home.page.vue
+++ src/pages/Home.page.vue
@@ -5,8 +5,8 @@
 <template>
   <div></div>
   <page-header/>
-  <main class = "flex flex-col items-center">
-    <h1>DevBlook page</h1>
+  <main class = "flex mt-16 text-2xl flex-col items-center">
+    <h1>Blogify page</h1>
     <p>WIP</p>
   </main>
 
