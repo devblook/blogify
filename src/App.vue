2564d0844d298608900e297b4b912f9037511158
BryanG <davidbryanrs@gmail.com>
2024-07-02 22:03:44 -0500

    feat(button): create global buttons (theme mode, language)

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <script setup>
+import ImageButton from '@/components/OptionButton.component.vue'
 
 </script>
 
 <template>
+  <div class = "fixed m-6 text-3xl flex flex-col gap-2.5">
+    <image-button>
+      <i class = "pi pi-sun"/>
+    </image-button>
+    <image-button>
+      <i class = "pi pi-language"/>
+    </image-button>
+  </div>
+
   <div class = "w-full flex flex-col items-center">
     <router-view/>
   </div>
