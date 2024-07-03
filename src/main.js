4cb881f70addca8c687d296d7b0354f8249c0840
BryanG <davidbryanrs@gmail.com>
2024-07-02 22:07:20 -0500

    fix: remove deleted component

--- src/main.js
+++ src/main.js
@@ -3,13 +3,10 @@ import 'primeicons/primeicons.css'
 
 import router from './router'
 
-import HeaderComponent from '@/components/Header.component.vue'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
 
 createApp(App)
-    .component("page-header", HeaderComponent)
     .use(router)
     .mount('#app')
