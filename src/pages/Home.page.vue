3014d31ec86fe87b513361662b2f4398f9662a7c
BryanG <davidbryanrs@gmail.com>
2024-07-04 00:07:36 -0500

    feat(page): implement language system to page

--- src/pages/Home.page.vue
+++ src/pages/Home.page.vue
@@ -1,7 +1,9 @@
 <script setup>
 
-import ImageButton from "@/components/OptionButton.component.vue";
+import RoundedButton from "@/components/RoundedButton.component.vue";
+
 import {darkMode, toggleColorTheme} from "@/scripts/color-scheme.js";
+import {changeLanguage, languageName}  from '@/scripts/i18n.js';
 
 </script>
 
@@ -10,32 +12,26 @@ import {darkMode, toggleColorTheme} from "@/scripts/color-scheme.js";
   <main class = "flex mt-24 w-page-limit min-w-72 flex-col gap-10">
     <div>
       <div class = "flex justify-between">
-        <h1 class = "text-4xl">Home</h1>
-        <div class = " computer:text-2xl mobile:text-xl flex gap-2.5">
-          <image-button v-on:click="toggleColorTheme">
-            <div v-if="darkMode">
-              <i class = "pi pi-moon"/>
-            </div>
-            <div v-else>
-              <i class = "pi pi-sun"/>
+        <h1 class = "text-4xl">{{$t('homeTitle')}}</h1>
+        <div :key="darkMode" class = " computer:text-2xl mobile:text-xl flex gap-2.5">
+          <rounded-button @click="toggleColorTheme">
+            <i class = "pi pi-moon"/>
 
-            </div>
-          </image-button>
+          </rounded-button>
 
-          <image-button>
-            <p>es</p>
-
-          </image-button>
+          <rounded-button @click="changeLanguage" :key="darkMode">
+            <p>{{languageName}}</p>
+          </rounded-button>
 
 
         </div>
       </div>
       <hr class = "my-5 border-gray-500">
-      <p>¡Welcome to this page!</p>
+      <p>{{$t('homeContent')}}</p>
     </div>
     <div class = "text-1xl flex gap-2.5 items-center">
       <i class = "pi pi-spin pi-spinner"/>
-      <p>Page in progress...</p>
+      <p>{{$t('pageStatus')}}</p>
 
     </div>
 
