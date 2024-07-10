7aa61f3119a35bc46eaede5cacdc8d14e3e6fd1d
BryanG <davidbryanrs@gmail.com>
2024-07-10 16:18:39 -0500

    refactor(page): change component system

--- src/pages/Home.page.vue
+++ src/pages/Home.page.vue
@@ -1,61 +1,22 @@
 <script setup>
 
-import RoundedButton from "@/components/RoundedButton.component.vue";
-
-import {darkMode, toggleColorTheme} from "@/scripts/color-scheme.js";
-import {changeLanguage, languageName}  from '@/scripts/i18n.js';
-
 </script>
 
 <template>
-  <div class = "mt-24 w-page-limit min-w-72 ">
 
-    <main class = "flex flex-col gap-10">
-      <div>
-        <div class = "flex justify-between">
-          <h1 class = "text-4xl">{{$t('home.title')}}</h1>
-          <div class = " computer:text-2xl mobile:text-xl flex gap-2.5">
-            <rounded-button @click="toggleColorTheme(!darkMode)">
-              <i v-if="darkMode" class = "pi pi-moon"/>
-              <i v-else class = "pi pi-sun"/>
-            </rounded-button>
-
-            <rounded-button @click="changeLanguage">
-              <p>{{languageName}}</p>
-            </rounded-button>
-
-
-          </div>
-        </div>
-        <hr class = "my-5 border-gray-500">
-        <div class = "flex flex-col gap-3 text-lg">
-          <p>{{$t('home.subtitle')}}</p>
-          <p>{{$t('home.content')}}</p>
-        </div>
+  <template-page page-name="home">
+    <div class = "flex gap-4">
+      <p class="whitespace-pre-line text-justify">{{$t('home.content')}}</p>
+    </div>
+    <div class = "flex flex-col gap-1">
+      <div class = "flex flex-col items-center">
+        <i class = "pi pi-cog text-3xl ml-36 pi-spin"/>
+        <i class = "pi pi-server text-blue-400 text-9xl"/>
       </div>
-      <div class = "flex flex-col gap-1">
-        <div class = "flex flex-col items-center">
-          <i class = "pi pi-cog text-3xl ml-36 pi-spin"/>
-          <i class = "pi pi-server text-blue-400 text-9xl"/>
-        </div>
-        <div class = "text-1.5xl flex gap-2.5 items-center justify-center">
-          <i class = "pi pi-spin pi-spinner"/>
-          <p class = "">{{$t('pageStatus')}}</p>
-        </div>
+      <div class = "text-1.5xl flex gap-2.5 items-center justify-center">
+        <i class = "pi pi-spin pi-spinner"/>
+        <p class = "">{{$t('pageStatus')}}</p>
       </div>
-
-    </main>
-    <hr class = "mt-5 mb-3 border-gray-500">
-    <footer class="flex justify-end">
-      <a href="https://github.com/devblook/blogin">
-        <i class = "pi pi-github text-3xl"></i>
-      </a>
-    </footer>
-  </div>
-
-
+    </div>
+  </template-page>
 </template>
-
-<style scoped>
-
-</style>
